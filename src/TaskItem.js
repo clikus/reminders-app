3af9e7b7cacb51850b6f7dbffdb8ff0e95ccc43f
clikus <cmclikus@gmail.com>
2020-02-11 12:20:09 +0800

    Rewrite TaskItem.js to FC

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -3,15 +3,13 @@ import React from "react";
 import { TaskName } from "./TaskName";
 import { TaskActions } from "./TaskActions";
 
-export class TaskItem extends React.Component {
-  render() {
-    const { task, index, editTask, deleteTask } = this.props;
+export function TaskItem(props) {
+  const { task, index, editTask, deleteTask } = props;
 
-    return (
-      <div className="task-item-container">
-        <TaskName task={task} index={index} editTask={editTask} />
-        <TaskActions task={task} index={index} editTask={editTask} deleteTask={deleteTask}/>
-      </div>
-    );
-  }
+  return (
+    <div className="task-item-container">
+      <TaskName task={task} index={index} editTask={editTask} />
+      <TaskActions task={task} index={index} editTask={editTask} deleteTask={deleteTask} />
+    </div>
+  );
 }
