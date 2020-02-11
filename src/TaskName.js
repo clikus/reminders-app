4667d9440fc3fd456b87d1361d31207b1701639b
clikus <cmclikus@gmail.com>
2020-02-11 16:41:50 +0800

    Rewrite css of TaskName.js to styletron

--- src/TaskName.js
+++ src/TaskName.js
@@ -1,4 +1,27 @@
-import React from "react";
+import React from 'react';
+
+import { styled } from 'styletron-react';
+
+const TaskNameContainer = styled('div', {
+  textAlign: 'left',
+  color: 'black'
+});
+
+const TaskNameForm = styled('form', {
+  display: 'flex',
+  alignItems: 'center'
+});
+
+const TaskCheckbox = styled('input', {
+  paddingRight: '20px',
+  width: '20px',
+  height: '20px'
+});
+
+const TaskLabel = styled('label', {
+  fontSize: '22px',
+  paddingLeft: '16px'
+});
 
 export function TaskName(props) {
   const { task, editTask , index} = props;
@@ -8,11 +31,11 @@ export function TaskName(props) {
   }
 
   return (
-    <div className="check-task">
-      <form className='task-name-form'>
-        <input className="task-checkbox" type="checkbox" name="taskBox" onChange={onToggleCheckbox} checked={!!task.isFinished}/>
-        <label className="task-name-label">{task.name}({task.place})</label>
-      </form>
-    </div>
+    <TaskNameContainer>
+      <TaskNameForm>
+        <TaskCheckbox type="checkbox" name="taskBox" onChange={onToggleCheckbox} checked={!!task.isFinished}/>
+        <TaskLabel>{task.name}({task.place})</TaskLabel>
+      </TaskNameForm>
+    </TaskNameContainer>
   );
 }
