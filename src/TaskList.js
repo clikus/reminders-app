efd9d2f779ebbab79cab1f4befc8688ae30e21f8
clikus <cmclikus@gmail.com>
2020-02-11 12:00:33 +0800

    Rewrite TaskList.js to FC

--- src/TaskList.js
+++ src/TaskList.js
@@ -2,14 +2,14 @@ import React from "react";
 
 import { TaskItem } from "./TaskItem";
 
-export class TaskList extends React.Component {
-  render() {
-    return (
-      <div className="task-list-container">
-        {this.props.tasks.map((task,index) => 
-          <TaskItem key={index} index={index} task={task} editTask={this.props.editTask} deleteTask={this.props.deleteTask}/>
-          )}
-      </div>
-    );
-  }
+export function TaskList (props){
+  const { editTask, deleteTask, tasks } = props;
+
+  return (
+    <div className="task-list-container">
+      { tasks.map((task,index) => 
+        <TaskItem key={index} index={index} task={task} editTask={editTask} deleteTask={deleteTask}/>
+      )}
+    </div>
+  );
 }
