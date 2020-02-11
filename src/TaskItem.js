0ad28668d4d4ba3415c68010859e584ae7a69756
clikus <cmclikus@gmail.com>
2020-02-11 16:31:30 +0800

    Rewrite css of TaskItem.js to Styletron

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import { TaskName } from "./TaskName";
-import { TaskActions } from "./TaskActions";
+import { TaskName } from './TaskName';
+import { TaskActions } from './TaskActions';
+import { styled } from 'styletron-react';
+
+const TaskItemContainer = styled('div', {
+  padding: '20px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center'
+});
 
 export function TaskItem(props) {
   const { task, index, editTask, deleteTask } = props;
 
   return (
-    <div className="task-item-container">
+    <TaskItemContainer>
       <TaskName task={task} index={index} editTask={editTask} />
       <TaskActions task={task} index={index} editTask={editTask} deleteTask={deleteTask} />
-    </div>
+    </TaskItemContainer>
   );
 }
