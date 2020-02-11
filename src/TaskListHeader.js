82539c206483459457a6f323e5a2b1afd5ed7473
clikus <cmclikus@gmail.com>
2020-02-11 11:16:03 +0800

    Made TaskListHeader.js into Functional Component

--- src/TaskListHeader.js
+++ src/TaskListHeader.js
@@ -1,27 +1,25 @@
 import React from "react";
 import * as TaskFilter from './taskFilter';
 
-export class TaskListHeader extends React.Component {
+export function TaskListHeader(props){
 
-    onRadioClick = event => {
-        const { setFilter } = this.props;
-        const { value } = event.target;
-        setFilter(value);
-    }
+  const { filter, setFilter } = props;
 
-    render() {
-      const { filter } = this.props;
-      return (
-        <div className="task-list-header-container">
-            <h2 className="task-label task-list-flex-item">Task List</h2>
-            <div>
-                <form>
-                    <input type="radio" onChange={this.onRadioClick} name="ALL" value={TaskFilter.ALL} checked={filter === TaskFilter.ALL}/> All 
-                    <input type="radio" onChange={this.onRadioClick} name="FINISHED" value={TaskFilter.FINISHED} checked={filter === TaskFilter.FINISHED}/> Finished
-                    <input type="radio" onChange={this.onRadioClick} name="TODO" value={TaskFilter.TODO} checked={filter === TaskFilter.TODO}/> To Do
-                </form>
-            </div>
-        </div>
-      );
-    }
-  } 
+  function onRadioClick(event){
+    const { value } = event.target;
+    setFilter(value);
+  }
+
+  return (
+    <div className="task-list-header-container">
+      <h2 className="task-label task-list-flex-item">Task List</h2>
+      <div>
+        <form>
+          <input type="radio" onChange={onRadioClick} name="ALL" value={TaskFilter.ALL} checked={filter === TaskFilter.ALL}/> All 
+          <input type="radio" onChange={onRadioClick} name="FINISHED" value={TaskFilter.FINISHED} checked={filter === TaskFilter.FINISHED}/> Finished
+          <input type="radio" onChange={onRadioClick} name="TODO" value={TaskFilter.TODO} checked={filter === TaskFilter.TODO}/> To Do
+        </form>
+      </div>
+    </div>
+  )
+}
