3b28d37af3091c0d1b42d59b0af54d0e27f1f33d
clikus <cmclikus@gmail.com>
2020-02-11 10:51:55 +0800

    Made AddTask.js into functional component

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,27 +1,29 @@
 import React from "react";
 
-export class AddTask extends React.Component {
+export function AddTask(props) {
 
-  onSubmitButtonClick = (event) => {
+  const { addTask } = props;
+
+  function onSubmitButtonClick (event){
     const newTaskName = event.currentTarget["add-task-input"].value;
     const newPlaceName = event.currentTarget["add-task-place"].value;
     //addTask Object to add Multiple variables ex. name & isChecked
-    this.props.addTask({
+    addTask({
       name: newTaskName,
       place: newPlaceName,
       isFinished: false
     });
-
+    
     event.preventDefault();
   }
 
-    render() {
-      return (
-        <form className="App" onSubmit={this.onSubmitButtonClick}>
-           <input className="add-task-input-tag" type="text" name="add-task-input" placeholder="Remind me to..."/>
-           <input className="add-task-place-tag" type="text" name="add-task-place" placeholder="Place of task"/>
-           <button className="add-task-button"><p>Add Task</p></button>
-        </form>
-      );
-    }
-  }
+  return (
+    <form className="App" onSubmit={onSubmitButtonClick}>
+      <input className="add-task-input-tag" type="text" name="add-task-input" placeholder="Remind me to..."/>
+      <input className="add-task-place-tag" type="text" name="add-task-place" placeholder="Place of task"/>
+      <button className="add-task-button">
+        <p>Add Task</p>
+      </button>
+    </form>
+  )
+}
