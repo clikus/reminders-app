fc2b2ea72214c5f890d591d2355deae343ba6d76
clikus <cmclikus@gmail.com>
2020-02-11 16:11:06 +0800

    Migrate css of AddTask.js to Styletron

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React from 'react';
+
+import {styled } from 'styletron-react';
+
+const AlignCenter = styled('form', {
+  textAlign: "center"
+});
+
+const AddTaskInputTag = styled('input', {
+  marginRight: '10px',
+  padding: '13px',
+  borderRadius: '5px',
+  width: '500px',
+  border: 'none'
+});
+
+const AddPlaceInputTag = styled('input', {
+  marginRight: '10px',
+  padding: '13px',
+  borderRadius: '5px',
+  width: '250px',
+  border: 'none'
+});
+
+const AddTaskButton = styled('button', {
+  marginLeft: '10px',
+  paddingLeft: '10px',
+  paddingRight: '10px',
+  backgroundColor: 'rgba(0, 252, 105, 0.74)',
+  color: 'white',
+  fontWeight: 'bold',
+  borderadius: '5px',
+  width: '100px',
+  borderWidth: '1px',
+  borderColor: 'rgba(0, 252, 105, 0.74)',
+  borderRight: 'none',
+  borderLeft: 'none'
+});
 
 export function AddTask (props) {
 
-const { addTask } = props;
+  const { addTask } = props;
 
   function onSubmitButtonClick (event) {
     const newTaskName = event.currentTarget["add-task-input"].value;
@@ -18,14 +55,11 @@ const { addTask } = props;
   }
 
   return (
-    <form className="App" onSubmit={onSubmitButtonClick}>
-        <input className="add-task-input-tag" type="text" name="add-task-input" placeholder="Remind me to..."/>
-        <input className="add-task-place-tag" type="text" name="add-task-place" placeholder="Place of task"/>
-        <button className="add-task-button">
-          <p>Add Task</p>
-        </button>
-    </form>
+    <AlignCenter onSubmit={onSubmitButtonClick}>
+        <AddTaskInputTag type="text" name="add-task-input" placeholder="Remind me to..."/>
+        <AddPlaceInputTag type="text" name="add-task-place" placeholder="Place of task"/>
+        <AddTaskButton><p>Add Task</p></AddTaskButton>
+    </AlignCenter>
   );
-
 }
 
