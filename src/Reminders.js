3f14f419d63801ccb2dec42639c435b1d091d46f
clikus <cmclikus@gmail.com>
2020-02-11 10:29:24 +0800

    Made Reminders.js to a functional component

--- src/Reminders.js
+++ src/Reminders.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-export class Reminders extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Reminders</h1>
-      </div>
-    );
-  }
+export function Reminders(props) {
+  return (
+    <div className="App">
+      <h1>Reminders</h1>
+    </div>
+  );
 }
+
