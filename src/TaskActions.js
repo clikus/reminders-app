097f7f5dc4fd01f6857fd600fac7d6e66cbe4779
clikus <cmclikus@gmail.com>
2020-02-12 10:20:00 +0800

    Rewrite css of TaskActions to Styletron

--- src/TaskActions.js
+++ src/TaskActions.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 import { EDButtons } from './EDButtons';
+import { styled } from 'styletron-react';
+
+const TaskButtons = styled('div', {
+  textAlign: 'right',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-around'
+});
 
 export function TaskActions(props){
   const { task, index, editTask, deleteTask } = props;
 
   return (
-  <div className="task-buttons">
+  <TaskButtons>
     <EDButtons task={task} index={index} editTask={editTask} deleteTask={deleteTask}/>
-  </div>
+  </TaskButtons>
   );
 }
